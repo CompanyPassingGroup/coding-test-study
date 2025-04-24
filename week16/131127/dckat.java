5c5f0167b8a5e62985ab228cdb77837abc7d97c9
Kim Daecheol <kdc9619@naver.com>
2025-04-24 16:00:19 +0900

    131127/할인 행사/38ms

--- week16/131127/dckat.java
+++ week16/131127/dckat.java
@@ -1,46 +1,50 @@
 import java.util.*;
 
 class Solution {
-    // 해당 날짜에 회원가입이 가능한지 판별
-    public boolean check(HashMap<String, Integer> wants, HashMap<String, Integer> discounts) {
-        for (String key: wants.keySet()) {
-            // 물품의 수량이 하나라도 다를 경우 false 리턴
-            if (wants.get(key) != discounts.get(key)) {
-                return false;
-            }
-        }
-        return true;
-    }
     
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        HashMap<String, Integer> wants = new HashMap<>();           // 구매를 원하는 물품과 수량
-        HashMap<String, Integer> discounts = new HashMap<>();       // 10일간 할인 상품과 전체 수
+        int matchCnt = 0;       // 구매 물품과 할인 물품의 일치 여부 확인 변수
+        HashMap<String, Integer> wants = new HashMap<>();
+        HashMap<String, Integer> discounts = new HashMap<>();
         
-        // 구매를 원하는 수 저장
+        // 구매희망 물품 갯수 저장
         for (int i = 0; i < number.length; i++) {
             wants.put(want[i], number[i]);
         }
         
-        // 1일 ~ 10일부터 먼저 수행하고 확인
-        // begin: 날짜가 하루 지날때 빠져야 할 물품 대상
+        // 첫날 부터 10일까지 할인 물품의 갯수 확인 (이 부분만 따로 빼서 해당 작업이 n번이 아닌 1번만 수행되도록 함)
         String begin = discount[0];
         for (int i = 0; i < 10; i++) {
             discounts.put(discount[i], discounts.getOrDefault(discount[i], 0) + 1);
+            
+            if (discounts.get(discount[i]) == wants.get(discount[i])) {
+                matchCnt++;
+            }
         }
         
-        if (check(wants, discounts)) {
+        if (matchCnt == wants.size()) {
             answer++;
         }
         
-        // 2일째부터 하루씩 shift 하면서 할인 물품 정보 갱신
-        // 갱신하면서 check 함수를 호출하여 회원가입 가능 여부 확인
         for (int i = 1; i < discount.length - 9; i++) {
+            // 이동하면서 할인에서 빠지는 물품의 갯수의 match 여부 비교 (빠지기 전 상태로 확인) 
+            if (discounts.get(begin) == wants.get(begin)) {
+                matchCnt--;
+            }
             discounts.put(begin, discounts.get(begin) - 1);
+            
+            // 추가되는 할인 물품의 추가 후 match 여부 비교
             discounts.put(discount[i+9], discounts.getOrDefault(discount[i+9], 0) + 1);
-            if (check(wants, discounts)) {
+            if (discounts.get(discount[i+9]) == wants.get(discount[i+9])) {
+                matchCnt++;
+            }
+            
+            // 구매 물품들과 할인 물품의 갯수가 일치
+            if (matchCnt == wants.size()) {
                 answer++;
             }
+            
             begin = discount[i];
         }
         
